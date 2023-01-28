3730ae17e58e6704cc0f4c8d775b39b568ab19d4
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-01-28 20:40:31 +0500

    TypeScript class

---
+++ app.ts
@@ -0,0 +1,22 @@
+// let inputUser = document.querySelector("#username") as HTMLInputElement;
+// let inputPass = document.querySelector("#password") as HTMLInputElement;
+// let htmlForm = document.querySelector(".form") as HTMLFormElement;
+
+// htmlForm.addEventListener("submit", () => {
+//     if (inputPass.value && inputUser.value) {
+
+// }
+// }) 
+class Car {
+    brand: string;
+    model: string;
+    constructor(brand: string, model: string) {
+        this.brand = brand;
+        this.model = model;
+    }
+    drive() {
+        console.log(this.model + " Car is enabled");
+
+    }
+}
+const myCar = new Car("Ferrari", "JIP" );
